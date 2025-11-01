650e19bed11ce27d02133fd6c026beb446e25a33
mariamatanji <38054680+mariamatanji@users.noreply.github.com>
2025-11-01 10:45:10 -0700

    Add DataChatToInt class for char to int conversion

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class DataChatToInt {
+    public static void main(String s[]) {
+        char name = 'a';
+        System.out.println("Name is  " + name);
+        System.out.println("Converted name is "+ (int)name);
+        
+    }
+}
